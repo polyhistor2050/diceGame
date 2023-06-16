f12271d074c14dc313fe7db1f0fe8322ebfe92be
polyhistor2050 <chrisspaul2050@gmail.com>
2023-06-16 13:25:53 +0300

    update player one to user input using prompt which allow multiplayer game between computer and human

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-//Generating random dice image for player 1;
-let randomNo1 = Math.floor(Math.random() * 6) + 1;
+//Prompt player for an input
+let randomNo1 = prompt("Enter Number From 1 -6");
+console.log(randomNo1);
 let randomDice1 = "assets/" + "dice" + randomNo1 + ".png";
 let player1 = document.querySelectorAll("img")[0];
 player1.setAttribute("src", randomDice1);
+
+
+if(randomNo1 > 6){
+    alert("Out of range, Please choose again");
+    randomNo1 = prompt("Enter Number From 1 -6");
+    let randomDice1 = "assets/" + "dice" + randomNo1 + ".png";
+    let player1 = document.querySelectorAll("img")[0];
+    player1.setAttribute("src", randomDice1);
+}
+
 
 
 //Generating random dice image for player 2;
@@ -14,10 +25,10 @@ let randomNo2 = Math.floor(Math.random() * 6) + 1;
 //Comparing dice value of player1 and player2 to get the winner 
 if (randomNo1 > randomNo2){
     let results = document.querySelector("h1");
-    results.innerHTML = "Player 1 Wins";
+    results.innerHTML = "You Win";
 }else if(randomNo2 > randomNo1){
     let results = document.querySelector("h1");
-    results.innerHTML = "Player 2 Wins";
+    results.innerHTML = "Computer Wins";
 }else {
     let results = document.querySelector("h1");
     results.innerHTML = "This is a draw";
